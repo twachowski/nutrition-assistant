e67f6747fea5db81ee1ef0c5ad32f36f283835f4
Tomasz Wachowski <tomo6916@wp.pl>
2019-12-19 21:34:17 +0100

    Remove password salt, set constant password length constraint

--- src/main/java/pl/polsl/wachowski/nutritionassistant/db/user/UserCredentials.java
+++ src/main/java/pl/polsl/wachowski/nutritionassistant/db/user/UserCredentials.java
@@ -11,13 +11,9 @@ public class UserCredentials {
     @Id
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false,
+            columnDefinition = "CHAR(60)")
     private String password;
-
-    @Column(unique = true,
-            nullable = false,
-            columnDefinition = "CHAR(6)")
-    private String passwordSalt;
 
     @OneToOne
     @MapsId
