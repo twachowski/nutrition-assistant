93ec064c91d1ab3ded8ebe715aecc2339fbd9b58
Tomasz Wachowski <tomo6916@wp.pl>
2020-01-14 21:08:40 +0100

    Add macronutrient support for existing providers

--- src/main/java/pl/polsl/wachowski/nutritionassistant/data/mapper/fdc/GeneralNutrientsMapper.java
+++ src/main/java/pl/polsl/wachowski/nutritionassistant/data/mapper/fdc/GeneralNutrientsMapper.java
@@ -18,6 +18,9 @@ public class GeneralNutrientsMapper implements NutrientMapper {
 
     private GeneralNutrientsMapper() {
         generalNutrientsMap.put(1008, GeneralNutrient.ENERGY);
+        generalNutrientsMap.put(1005, GeneralNutrient.TOTAL_CARBS);
+        generalNutrientsMap.put(1004, GeneralNutrient.TOTAL_FAT);
+        generalNutrientsMap.put(1003, GeneralNutrient.TOTAL_PROTEIN);
         generalNutrientsMap.put(1051, GeneralNutrient.WATER);
         generalNutrientsMap.put(1057, GeneralNutrient.CAFFEINE);
         generalNutrientsMap.put(1018, GeneralNutrient.ALCOHOL);
--- src/main/java/pl/polsl/wachowski/nutritionassistant/data/mapper/nutritionix/GeneralNutrientsMapper.java
+++ src/main/java/pl/polsl/wachowski/nutritionassistant/data/mapper/nutritionix/GeneralNutrientsMapper.java
@@ -18,6 +18,9 @@ public final class GeneralNutrientsMapper implements NutrientMapper {
 
     private GeneralNutrientsMapper() {
         generalNutrientsMap.put(208, GeneralNutrient.ENERGY);
+        generalNutrientsMap.put(205, GeneralNutrient.TOTAL_CARBS);
+        generalNutrientsMap.put(204, GeneralNutrient.TOTAL_FAT);
+        generalNutrientsMap.put(203, GeneralNutrient.TOTAL_PROTEIN);
         generalNutrientsMap.put(255, GeneralNutrient.WATER);
         generalNutrientsMap.put(262, GeneralNutrient.CAFFEINE);
         generalNutrientsMap.put(221, GeneralNutrient.ALCOHOL);
