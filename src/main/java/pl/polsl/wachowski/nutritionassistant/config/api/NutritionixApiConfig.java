07744e4fa7ecd49d86078e02c1375f71b64e87d6
Tomasz Wachowski <tomo6916@wp.pl>
2020-01-14 23:20:57 +0100

    Move Nutritionix API header configuration to properties file

--- src/main/java/pl/polsl/wachowski/nutritionassistant/config/api/NutritionixApiConfig.java
+++ src/main/java/pl/polsl/wachowski/nutritionassistant/config/api/NutritionixApiConfig.java
@@ -4,8 +4,10 @@ import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.http.HttpHeaders;
 
-@Getter
+import javax.annotation.PostConstruct;
+
 @Configuration
 @PropertySource("classpath:nutrition-api.properties")
 public class NutritionixApiConfig {
@@ -16,13 +18,32 @@ public class NutritionixApiConfig {
     @Value("${nutritionix-api.key}")
     private String apiKey;
 
+    @Getter
     @Value("${nutritionix-api.instant.url}")
     private String instantUrl;
 
+    @Getter
     @Value("${nutritionix-api.nutrients.url}")
     private String nutrientsUrl;
 
+    @Getter
     @Value("${nutritionix-api.exercise.url}")
     private String exerciseUrl;
+
+    @Value("${nutritionix-api.app-id.header}")
+    private String appIdHeaderName;
+
+    @Value("${nutritionix-api.key.header}")
+    private String apiKeyHeaderName;
+
+    @Getter
+    private HttpHeaders headers;
+
+    @PostConstruct
+    private void initializeHeaders() {
+        headers = new HttpHeaders();
+        headers.set(appIdHeaderName, appId);
+        headers.set(apiKeyHeaderName, apiKey);
+    }
 
 }
--- src/main/java/pl/polsl/wachowski/nutritionassistant/data/provider/nutritionix/NutritionixProvider.java
+++ src/main/java/pl/polsl/wachowski/nutritionassistant/data/provider/nutritionix/NutritionixProvider.java
@@ -2,7 +2,6 @@ package pl.polsl.wachowski.nutritionassistant.data.provider.nutritionix;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -16,27 +15,22 @@ import pl.polsl.wachowski.nutritionassistant.dto.search.nutritionix.NutritionixS
 @Component
 public class NutritionixProvider implements FoodDataProvider<NutritionixSearchResultDTO, NutritionixFoodDetailsDTO> {
 
-    private static final String APP_ID_HEADER_NAME = "x-app-id";
-    private static final String API_KEY_HEADER_NAME = "x-app-key";
     private static final String SERVING_100G = "100g";
 
     private final NutritionixApiConfig config;
 
     private final RestTemplate restTemplate;
 
-    private final HttpHeaders headers;
-
     @Autowired
     public NutritionixProvider(final NutritionixApiConfig config, final RestTemplate restTemplate) {
         this.config = config;
         this.restTemplate = restTemplate;
-        this.headers = createHeaders();
     }
 
     @Override
     public NutritionixSearchResultDTO search(final String query) {
         final String url = String.format(config.getInstantUrl(), query);
-        final HttpEntity request = new HttpEntity<>(null, headers);
+        final HttpEntity request = new HttpEntity<>(null, config.getHeaders());
         final ResponseEntity<NutritionixSearchResultDTO> result =
                 restTemplate.exchange(url, HttpMethod.GET, request, NutritionixSearchResultDTO.class);
         return result.getBody();
@@ -47,15 +41,8 @@ public class NutritionixProvider implements FoodDataProvider<NutritionixSearchRe
         final String url = config.getNutrientsUrl();
         final String query = String.join(" ", id, SERVING_100G);
         final NutritionixFoodDetailsRequestBodyDTO body = new NutritionixFoodDetailsRequestBodyDTO(query);
-        final HttpEntity<NutritionixFoodDetailsRequestBodyDTO> request = new HttpEntity<>(body, headers);
+        final HttpEntity<NutritionixFoodDetailsRequestBodyDTO> request = new HttpEntity<>(body, config.getHeaders());
         return restTemplate.postForObject(url, request, NutritionixFoodDetailsDTO.class);
-    }
-
-    private HttpHeaders createHeaders() {
-        final HttpHeaders headers = new HttpHeaders();
-        headers.set(APP_ID_HEADER_NAME, config.getAppId());
-        headers.set(API_KEY_HEADER_NAME, config.getApiKey());
-        return headers;
     }
 
 }
