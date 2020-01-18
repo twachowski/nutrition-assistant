6394f149d2995b7ca1aa158dc43a3115a796a670
Tomasz Wachowski <tomo6916@wp.pl>
2020-01-18 20:54:32 +0100

    Define specific omega-3 fatty acids

--- src/main/java/pl/polsl/wachowski/nutritionassistant/def/nutrition/Lipid.java
+++ src/main/java/pl/polsl/wachowski/nutritionassistant/def/nutrition/Lipid.java
@@ -7,8 +7,12 @@ public enum Lipid implements Nutrient {
     SATURATED_FAT(MassUnit.GRAM),
     MONOUNSATURATED_FAT(MassUnit.GRAM),
     POLYUNSATURATED_FAT(MassUnit.GRAM),
-    OMEGA6(MassUnit.GRAM),
-    OMEGA3(MassUnit.GRAM),
+    OMEGA6(MassUnit.GRAM),      //linoleic acid only
+    OMEGA3_ALA(MassUnit.GRAM),
+    OMEGA3_DPA(MassUnit.GRAM),
+    OMEGA3_EPA(MassUnit.GRAM),
+    OMEGA3_DHA(MassUnit.GRAM),
+    OMEGA3(MassUnit.GRAM),      //total
     TRANS_FAT(MassUnit.GRAM),
     CHOLESTEROL(MassUnit.MILLIGRAM);
 
