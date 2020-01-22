0b3672ae715fce062be2c19d6c0f996f9a6812ee
Tomasz Wachowski <tomo6916@wp.pl>
2020-01-22 16:40:07 +0100

    Add missing annotation to token repository

--- src/main/java/pl/polsl/wachowski/nutritionassistant/repository/TokenRepository.java
+++ src/main/java/pl/polsl/wachowski/nutritionassistant/repository/TokenRepository.java
@@ -1,9 +1,11 @@
 package pl.polsl.wachowski.nutritionassistant.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pl.polsl.wachowski.nutritionassistant.db.user.User;
 import pl.polsl.wachowski.nutritionassistant.db.user.VerificationToken;
 
+@Repository
 public interface TokenRepository extends JpaRepository<VerificationToken, Long> {
 
     VerificationToken findVerificationTokenByValue(final String token);
