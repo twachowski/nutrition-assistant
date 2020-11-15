8dbc6a1948faa5d5601a33875bc5eb34bfceee25
Tomasz Wachowski <tomo6916@wp.pl>
2020-11-16 00:17:15 +0100

    Update FDC API endpoints

--- clients/fdc-client/src/main/java/pl/polsl/wachowski/fdc/client/api/FdcApi.java
+++ clients/fdc-client/src/main/java/pl/polsl/wachowski/fdc/client/api/FdcApi.java
@@ -10,7 +10,12 @@ public final class FdcApi {
     public static final String API_KEY = "api_key";
     public static final String FOOD_ID = "{foodId}";
 
-    public static final String SEARCH_API = API_PREFIX + "/search";
-    public static final String FOOD_API = API_PREFIX + "/" + FOOD_ID;
+    public static final String FOODS_API_SUFFIX = "/foods";
+    public static final String FOOD_API_SUFFIX = "/food";
+    public static final String FOODS_SEARCH_API_SUFFIX = FOODS_API_SUFFIX + "/search";
+    public static final String FOOD_ID_API_SUFFIX = FOOD_API_SUFFIX + "/" + FOOD_ID;
+
+    public static final String SEARCH_API = API_PREFIX + FOODS_SEARCH_API_SUFFIX;
+    public static final String FOOD_API = API_PREFIX + FOOD_ID_API_SUFFIX;
 
 }
