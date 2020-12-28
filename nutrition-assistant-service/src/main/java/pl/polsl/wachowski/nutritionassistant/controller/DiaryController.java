505a2de1204077b098aefa3cb65ff224c9d3e814
Tomasz Wachowski <tomo6916@wp.pl>
2020-12-28 07:06:59 +0100

    Fix conversion problems with LocalDate path variables

--- nutrition-assistant-service/src/main/java/pl/polsl/wachowski/nutritionassistant/controller/DiaryController.java
+++ nutrition-assistant-service/src/main/java/pl/polsl/wachowski/nutritionassistant/controller/DiaryController.java
@@ -1,6 +1,7 @@
 package pl.polsl.wachowski.nutritionassistant.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -28,14 +29,16 @@ public class DiaryController {
     }
 
     @GetMapping(produces = APPLICATION_JSON_VALUE)
-    ResponseEntity<DiaryEntriesResponse> getEntries(@PathVariable(DATE) final LocalDate diaryDate) {
+    ResponseEntity<DiaryEntriesResponse> getEntries(@PathVariable(DATE)
+                                                    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) final LocalDate diaryDate) {
         final DiaryEntriesResponse response = diaryFacade.getEntries(diaryDate);
         return ResponseEntity.ok(response);
     }
 
     @PostMapping(path = ENTRIES_API_SUFFIX,
                  consumes = APPLICATION_JSON_VALUE)
-    ResponseEntity<Void> addEntry(@PathVariable(DATE) final LocalDate diaryDate,
+    ResponseEntity<Void> addEntry(@PathVariable(DATE)
+                                  @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) final LocalDate diaryDate,
                                   @RequestBody @Valid final NewEntryRequest request) {
         diaryFacade.addEntry(diaryDate, request);
         return ResponseEntity.status(HttpStatus.CREATED)
@@ -44,7 +47,8 @@ public class DiaryController {
 
     @PatchMapping(path = ENTRIES_POSITION_API_SUFFIX,
                   consumes = APPLICATION_JSON_VALUE)
-    ResponseEntity<Void> editEntry(@PathVariable(DATE) final LocalDate diaryDate,
+    ResponseEntity<Void> editEntry(@PathVariable(DATE)
+                                   @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) final LocalDate diaryDate,
                                    @PathVariable(ENTRY_POSITION) final short entryPosition,
                                    @RequestBody @Valid final EntryEditRequest request) {
         diaryFacade.editEntry(diaryDate, entryPosition, request);
@@ -52,7 +56,8 @@ public class DiaryController {
     }
 
     @DeleteMapping(path = ENTRIES_POSITION_API_SUFFIX)
-    ResponseEntity<Void> deleteEntry(@PathVariable(DATE) final LocalDate diaryDate,
+    ResponseEntity<Void> deleteEntry(@PathVariable(DATE)
+                                     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) final LocalDate diaryDate,
                                      @PathVariable(ENTRY_POSITION) final short entryPosition) {
         diaryFacade.deleteEntry(diaryDate, entryPosition);
         return ResponseEntity.noContent().build();
@@ -60,7 +65,8 @@ public class DiaryController {
 
     @PatchMapping(path = ENTRIES_API_SUFFIX,
                   consumes = APPLICATION_JSON_VALUE)
-    ResponseEntity<Void> moveEntry(@PathVariable(DATE) final LocalDate diaryDate,
+    ResponseEntity<Void> moveEntry(@PathVariable(DATE)
+                                   @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) final LocalDate diaryDate,
                                    @RequestBody @Valid final EntryMoveRequest request) {
         diaryFacade.moveEntry(diaryDate, request);
         return ResponseEntity.noContent().build();
